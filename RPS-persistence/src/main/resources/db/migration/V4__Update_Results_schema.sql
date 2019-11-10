f8a3641653680feed05d18c8a572fc9d98fdba09
Uebel <lawrence.uebel@accenture.com>
2019-11-10 18:59:39 -0500

    h2 working

--- RPS-persistence/src/main/resources/db/migration/V4__Update_Results_schema.sql
+++ RPS-persistence/src/main/resources/db/migration/V4__Update_Results_schema.sql
@@ -1,6 +1,6 @@
 Alter table GAME_RESULTS drop column player1Name;
 Alter table GAME_RESULTS drop column player2Name;
-Alter table GAME_RESULTS modify column player1Id int;
-Alter table GAME_RESULTS modify column player2Id int;
+Alter table GAME_RESULTS alter column player1Id int;
+Alter table GAME_RESULTS alter column player2Id int;
 Alter table GAME_RESULTS add column player1Throw varchar(20);
 Alter table GAME_RESULTS add column player2Throw varchar(20);
