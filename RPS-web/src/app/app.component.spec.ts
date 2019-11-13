9fbfaa39194c216b20ed2a0814b4a6342c954557
Uebel <lawrence.uebel@accenture.com>
2019-11-13 09:17:09 -0500

    Revert "reset web"
    This reverts commit 56c8818848a863d0dcdfafd067235efe6406f5b2.

--- RPS-web/src/app/app.component.spec.ts
+++ RPS-web/src/app/app.component.spec.ts
@@ -1,75 +1,34 @@
-import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { SidebarComponent } from './sidebar/sidebar.component';
 import { AppComponent } from './app.component';
-import { StubRpsGateway } from './stub.rps.gateway';
-import { RpsGateway } from './rps.gateway';
-import { FormsModule } from '@angular/forms';
 
 
 describe('AppComponent', () => {
-
-  let stubRpsGateway: StubRpsGateway;
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
 
   beforeEach(async(() => {
-
-    stubRpsGateway = new StubRpsGateway();
-
     TestBed.configureTestingModule({
-      imports: [ FormsModule ],
-      declarations: [
-        AppComponent
-      ],
-      providers: [
-        { provide: RpsGateway, useValue: stubRpsGateway }
-      ]
-    }).compileComponents();
+      declarations: [ AppComponent ]
+    })
+    .compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-  describe('When the component loads', () => {
-    
-    it('should create the app', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.debugElement.componentInstance;
-      expect(app).toBeTruthy();
-    });
+  fit('should show options when you click the hamburger', () => {
+    const beforeClick = component.sidebarOpen;
+    const hamburger = fixture.nativeElement.querySelector('#hamburger-stack');
+    hamburger.click();
+    const afterClick = component.sidebarOpen;
+    expect(beforeClick).not.toEqual(afterClick);
 
-    it(`should have as title 'Rock, Paper, Scissors'`, () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.debugElement.componentInstance;
-      expect(app.title).toEqual('Rock, Paper, Scissors');
-    });
-
-    it('should render title in a h1 tag ', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      fixture.detectChanges();
-      const compiled = fixture.debugElement.nativeElement;
-      expect(compiled.querySelector('h1').textContent).toContain('Welcome to Rock, Paper, Scissors!');
-    });
-
-    it('should send game request to gateway', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      fixture.detectChanges();
-      const compiled = fixture.debugElement.nativeElement;
-      const p1Input: HTMLInputElement = compiled.querySelector('select[id=player1Throw]');
-      p1Input.value = 'ROCK';
-      p1Input.dispatchEvent( new Event('change'));
-      
-      const p2Input: HTMLInputElement = compiled.querySelector('select[id=player2Throw]');
-      p2Input.value = 'PAPER';
-      p2Input.dispatchEvent( new Event('change'));
-      
-      let button = compiled.querySelector('button');
-      button.click();
-      fixture.detectChanges();
-    
-      fixture.whenStable().then( () => {
-        console.log( 'In Stable');
-        expect(stubRpsGateway.savePlayPraticeGameCalledWith.player1Throw).toBe('ROCK');
-        expect(stubRpsGateway.savePlayPraticeGameCalledWith.player2Throw).toBe('PAPER');
-      });
-      //field.dispatchEvent( new Event( 'input'));
-      expect(compiled.querySelector('h1').textContent).toContain('Welcome to Rock, Paper, Scissors!');
-    });
+    expect(fixture.nativeElement.querySelector('#sidebar').textContent()).toContain('Play Game');
 
   });
+
 });
--- RPS-web/src/app/app.module.ts
+++ RPS-web/src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { RpsGateway } from './rps.gateway';
-import { HttpRpsGateway } from './http.rps.gateway';
+import { RpsGateway } from './game/game.gateway';
+import { HttpRpsGateway } from './game/http.game.gateway';
+import { RankedGameComponent } from './game/ranked-game/ranked-game.component';
+import { GameComponent } from './game/game.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RankedGameComponent,
+    GameComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule
   ],
   providers: [
